17d95de7b0ff77b9b2b9cc040c2cd94bd2629dfb
J-mot3000 <jmoton@icstars.org>
2024-09-16 07:02:25 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import EmployeeForm from './EmployeeForm';
+import EmployeeList from './EmployeeList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [employees, setEmployees] = useState([]);
+    const [darkMode, setDarkMode] = useState(false);
+
+    const fetchEmployees = async () => {
+        const response = await axios.get('http://localhost:3001/employees');
+        setEmployees(response.data);
+    };
+
+    useEffect(() => {
+        fetchEmployees();
+    }, []);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <div className={darkMode ? 'container dark-mode' : 'container'}>
+            <h1 className="my-4">Employee Database</h1>
+            <button className="btn btn-secondary mb-3" onClick={toggleDarkMode}>
+                {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
+            <EmployeeForm fetchEmployees={fetchEmployees} />
+            <EmployeeList employees={employees} fetchEmployees={fetchEmployees} />
+        </div>
+    );
+};
 
 export default App;
